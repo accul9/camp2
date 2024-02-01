7f5f899611ba87c9777e97d98be7f073efc76a60
Aichi Chen <elsie.720@outlook.com>
2024-02-02 01:16:54 +0900

    tailwindを追加設定した。 安全性補修2つを自動で実行した。

--- resources/views/sets/index.blade.php
+++ resources/views/sets/index.blade.php
@@ -1,3 +1,9 @@
+<nav class="bg-slate-500">ここは暫定のナビゲーター</nav>
+<main>
+    <h1>今日はどんなセットの気分？</h1>
+    <p>テスト</p>
+</main>
+
 @foreach ($sets as $set)
     <p>{{ $set->set_name }}</p>
     <div><a href="{{ route('sets.show', $set) }}"><img src="{{ asset($set->set_image) }}" alt="Set Image"></a></div>
