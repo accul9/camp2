6d3009fb800383a662db571870cb35202ff35701
Aichi Chen <elsie.720@outlook.com>
2024-02-09 15:32:41 +0900

    商品一覧のviewを作成

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,27 +1,39 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
+{{-- ↑テンプレを読み込む --}}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>個別商品一覧</title>
-    @vite('resources/css/app.css')
-</head>
 
-<body>
-    <nav class="w-full h-20 p-5 text-2xl text-teal-500 bg-slate-700">ここは暫定のナビゲーター</nav>
-    <main class="">
-        <h1 class="">個別商品一覧</h1>
-        <div>
+@section('content')
+    {{-- ここから編集して --}}
+    <div class="container w-full px-5 py-24 mx-auto mt-5">
+        {{-- 上半部分 --}}
+        <div class="flex flex-wrap w-full mb-20">
+            <div class="w-full mb-6 lg:w-1/2 lg:mb-0">
+                <h1 class="mb-2 text-2xl font-bold text-gray-900 sm:text-3xl title-font">
+                    個別商品一覧</h1>
+                <div class="w-40 h-1 bg-green-500 rounded"></div>
+            </div>
+            <p class="w-full leading-relaxed text-gray-500 lg:w-1/2">
+                こだわりの商品を取り揃えております。自分の好みに合った商品を見つけてください。
+            </p>
+        </div>
+        {{-- 下半部分 --}}
+        <div class="flex flex-row flex-wrap p-4 -m-4">
             @foreach ($items as $item)
-                <p>{{ $item->item_name }}</p>
-                <div><a href="{{ route('items.show', $item) }}"><img src="{{ asset($item->item_image) }}"
-                            alt="Item Image"></a>
+                <div class="p-4 xl:w-1/4 md:w-1/3">
+                    <div class="p-10 rounded-lg shadow-lg bg-gray-50">
+                        <img class="object-cover object-center w-[200px] h-[200px] rounded-t-lg"
+                            src="{{ asset($item->item_image) }}" alt="content">
+                        <div class="pt-4 pb-8 rounded-b-lg bg-grey-50">
+                            <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">{{ $item->item_name }}</h2>
+                            <a href="{{ route('items.show', $item) }}">
+                                <h3 class="text-xs font-medium tracking-widest text-green-500 title-font">詳細を見る</h3>
+                            </a>
+                            {{-- <p class="text-base leading-relaxed">{{ $set->set_name }}</p> --}}
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </div>
-    </main>
-</body>
-
-</html>
+    </div>
+    {{-- ここまで編集して --}}
+@endsection
