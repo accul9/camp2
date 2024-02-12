015fb8ce29e87e5ad6c4bca499f332da37ad2f9b
Aichi Chen <elsie.720@outlook.com>
2024-02-12 22:44:57 +0900

    paginationのcomponentsを導入、商品一覧のページを分割

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -12,7 +12,7 @@ class ItemController extends Controller
      */
     public function index()
     {
-        $items = Item::all();
+        $items = Item::paginate(12);
         return view('items.index', compact('items'));
     }
 
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,39 +1,62 @@
 @extends('layouts.app')
-{{-- ↑テンプレを読み込む --}}
-
 
 @section('content')
     {{-- ここから編集して --}}
     <div class="container w-full px-5 py-24 mx-auto mt-5">
         {{-- 上半部分 --}}
-        <div class="flex flex-wrap w-full mb-20">
+        <div class="flex flex-wrap w-full pl-4 mb-20">
             <div class="w-full mb-6 lg:w-1/2 lg:mb-0">
-                <h1 class="mb-2 text-2xl font-bold text-gray-900 sm:text-3xl title-font">
-                    個別商品一覧</h1>
-                <div class="w-40 h-1 bg-green-500 rounded"></div>
+                <x-generic-h1>個別商品一覧</x-generic-h1>
             </div>
-            <p class="w-full leading-relaxed text-gray-500 lg:w-1/2">
-                こだわりの商品を取り揃えております。自分の好みに合った商品を見つけてください。
-            </p>
         </div>
         {{-- 下半部分 --}}
-        <div class="flex flex-row flex-wrap p-4 -m-4">
-            @foreach ($items as $item)
-                <div class="p-4 xl:w-1/4 md:w-1/3">
-                    <div class="p-10 rounded-lg shadow-lg bg-gray-50">
-                        <img class="object-cover object-center w-[270px] h-[270px] rounded-t-lg"
-                            src="{{ asset($item->item_image) }}" alt="content">
-                        <div class="pt-4 pb-4 rounded-b-lg bg-grey-50">
-                            <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">{{ $item->item_name }}</h2>
-                            <a href="{{ route('items.show', $item) }}">
-                                <h3 class="text-xs font-medium tracking-widest text-green-500 title-font">詳細を見る</h3>
-                            </a>
-                            {{-- <p class="text-base leading-relaxed">{{ $set->set_name }}</p> --}}
+        <div class="flex flex-row w-full">
+            <div class="flex flex-col w-1/4 p-8 m-4 rounded-xl bg-gray-50">
+                <x-generic-h2>Categories</x-generic-h2>
+                {{-- <ul>
+                    @foreach ($categories as $category)
+                        <a href="{{ route('categories.show', ['category_id' => $category->id]) }}">
+                            <li class="p-2 mb-4 text-gray-900 hover:bg-[#eff5d7] hover:text-xl rounded-lg">
+                                {{ $category->name }}
+                            </li>
+                        </a>
+                    @endforeach
+                </ul> --}}
+                {{-- <a href="{{ route('categories.show', ['category_id' => 1]) }}">Test Category Link</a> --}}
+                {{-- <ul>
+                    @foreach ($categories as $category)
+                        <a href="#">
+                            <li class="p-2 mb-4 text-gray-900 hover:bg-[#eff5d7] hover:text-xl rounded-lg">
+                                {{ $category->name }}
+                            </li>
+                        </a>
+                    @endforeach
+                </ul> --}}
+
+            </div>
+            <div class="flex flex-row flex-wrap w-3/4 p-4 -m-4">
+                @foreach ($items as $item)
+                    <div class="p-4 xl:w-1/1 md:w-1/3">
+                        <div class="p-8 rounded-lg shadow-lg bg-gray-50">
+                            <img class="object-cover object-center w-[270px] h-[270px] rounded-t-lg"
+                                src="{{ asset($item->item_image) }}" alt="content">
+                            <div class="pt-4 pb-4 rounded-b-lg bg-grey-50">
+                                <h2 class="mb-4 text-lg font-medium text-gray-900 title-font">{{ $item->item_name }}</h2>
+                                <a href="{{ route('items.show', $item) }}">
+                                    <h3 class="text-xs font-medium tracking-widest text-green-500 title-font">詳細を見る</h3>
+                                </a>
+                                {{-- <p class="text-base leading-relaxed">{{ $set->set_name }}</p> --}}
+                            </div>
                         </div>
                     </div>
+                @endforeach
+                <div class="mt-3">
+                    {{ $items->links() }}
                 </div>
-            @endforeach
+
+            </div>
         </div>
+
     </div>
     {{-- ここまで編集して --}}
 @endsection
