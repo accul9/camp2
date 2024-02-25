f5734b33bb3b3a1313e2b93aa0a66853a71473bc
Aichi Chen <elsie.720@outlook.com>
2024-02-25 19:32:51 +0900

    add set_id column to carts table

---
+++ database/migrations/2024_02_25_192829_add_set_id_to_carts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->unsignedBigInteger('set_id')->nullable()->after('item_id');
+            $table->foreign('set_id')->references('set_id')->on('sets')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->dropForeign(['set_id']);
+            $table->dropColumn('set_id');
+        });
+    }
+};
