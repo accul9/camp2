3ae9c3c77a3a8d4312f4650781dfbd20fe26985c
Aichi Chen <elsie.720@outlook.com>
2024-02-27 03:47:38 +0900

    email address change

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\ContactsSendmail;
 
 class ContactsController extends Controller
 {
@@ -23,9 +24,39 @@ class ContactsController extends Controller
         $inputs = $request->all();
 
         return view('contact.confirm', compact('inputs'));
-    }/* public function confirm(Request $request)
+    }
+
+    public function send(Request $request)
     {
-        $contact = $request->all();
-        return view('contact.confirm', compact('contact'));
-    } */
+        $request->validate([
+            'email' => 'required|email',
+            'name' => 'required',
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        //actionの値を取得
+        $action = $request->input('action');
+
+        //action以外の値を取得
+        $inputs = $request->except('action');
+
+        //actionの値で分岐
+        if ($action !== 'submit') {
+            //修正ボタンが押された時
+            return redirect()
+                ->route('contact.index')
+                ->withInput($inputs);
+        } else {
+            //メール送信処理
+            \Mail::to($inputs['email'])
+                ->send(new ContactsSendmail($inputs));
+            \Mail::to(config('mail.from.address'))
+                ->send(new ContactsSendmail($inputs));
+            //二重送信を防ぐためにトークンを再発行
+            $request->session()->regenerateToken();
+            //送信完了ページへのリダイレクト
+            return redirect()->route('contact.thanks');
+        }
+    }
 }
