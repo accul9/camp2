b5d8176cc45db8b073df5bdc8838a12822f69a2d
Takahata Yoshikazu <yoshi.lepus4423@gmail.com>
2024-03-03 20:07:09 +0900

    loginページの再修正、bg-cover,bg-center,bg-no-repeatの追加、入力欄の修正

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -4,9 +4,9 @@
     {{ $attributes->merge([
         'class' => 'block 
                    font-medium 
-                   text-sm 
-                   text-gray-700 
-                   dark:text-gray-200'
+                   text-sm
+                   text-gray-900 
+                   dark:text-gray-900'
     ]) 
     }}
 >
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -3,15 +3,14 @@
 <input 
     {{ $disabled ? 'disabled' : '' }} 
     {!! $attributes->merge([
-        'class' => 'border-gray-300 
-                   dark:border-gray-900 
-                   dark:bg-gray-900 
+        'class' => 'border-gray-600 
+                   dark:border-gray-600  
                    dark:text-gray-00 
                    focus:border-indigo-500 
                    dark:focus:border-indigo-600 
                    focus:ring-indigo-500 
                    dark:focus:ring-indigo-600 
-                   rounded-md 
-                   shadow-sm'
+                   rounded-3xl
+                   shadow-md'
     ]) !!}
 >
